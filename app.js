099b78b2161fcc77303a9d0ab7a3b8ff550482b5
Caspar Wylie <casparwylie@Caspars-MBP.home>
2015-12-25 12:11:41 +0000

    db stuff

--- app.js
+++ app.js
@@ -50,6 +50,14 @@ var db = mysql.createConnection({
 	database : "simplysuggest",
 });
 
+db.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + db.threadId);
+});
 //main functionality
 app.get('/', function(req,res){
 	index.index(req,res,db);
